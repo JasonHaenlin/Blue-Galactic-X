f4d68404203a6078b264d8d7e0c5207dc22de526
JYassine <yassine.jrad@etu.unice.fr>
2020-10-05 19:38:43 +0200

    [#44] init repo telemetry ws
    - integrated in CI
    - integrated in docker
    - using port 8090 8091

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-for i in "team-j-rocket-ws" "team-j-weather-ws" "team-j-mission-ws" "team-j-client"
+for i in "team-j-rocket-ws" "team-j-weather-ws" "team-j-mission-ws" "team-j-telemetry-ws" "team-j-client" 
 do
     cd $i; ./mvnw clean package -DskipTests; cd ..
 done
