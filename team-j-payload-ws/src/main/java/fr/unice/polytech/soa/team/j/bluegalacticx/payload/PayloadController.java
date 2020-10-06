4962817f31bebf4c6369c6b941cdc0f88c18a79d
Betsara Marcellin <betsara.marcellin@etu.unice.fr>
2020-10-06 22:07:14 +0200

    GH-25 add post and get payload

--- team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/PayloadController.java
+++ team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/PayloadController.java
@@ -1,17 +1,42 @@
 package fr.unice.polytech.soa.team.j.bluegalacticx.payload;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.entities.Payload;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.exceptions.InvalidPayloadException;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.exceptions.PayloadNotFoundException;
 
 @RestController
-@RequestMapping("/telemetry")
+@RequestMapping("/payload")
 public class PayloadController {
 
     @Autowired
     private PayloadService payloadService;
 
-  
-    
+    @PostMapping()
+    public Payload createNewPayload(@RequestBody Payload payload) {
+        try {
+            return payloadService.createPayload(payload);
+        } catch (InvalidPayloadException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Payload getPayload(@PathVariable String id) {
+        try {
+            return payloadService.retrievePayload(id);
+        } catch (PayloadNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
 
 }
---
+++ team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/exceptions/InvalidPayloadException.java
@@ -0,0 +1,14 @@
+package fr.unice.polytech.soa.team.j.bluegalacticx.payload.exceptions;
+
+public class InvalidPayloadException extends Exception {
+
+    /**
+     * auto-generated
+     */
+    private static final long serialVersionUID = 4133499328083489405L;
+    
+    public InvalidPayloadException() {
+        super("You cannot create a payload with those parameters, please change and retry");
+    }
+    
+}
