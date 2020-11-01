b4176fcf87c425ba821e222d3ec10f3382016c9b
Betsara-MARCELLIN <43007842+Betsara-MARCELLIN@users.noreply.github.com>
2020-11-01 15:42:57 +0100

    #128 add route route to create booster and get it

--- team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/BoosterController.java
+++ team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/BoosterController.java
@@ -2,12 +2,18 @@ package fr.unice.polytech.soa.team.j.bluegalacticx.booster;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.Booster;
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.BoosterDoesNotExistException;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.BoosterNotAvailableException;
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.CannotBeNullException;
 
 @RestController
 @RequestMapping("/booster")
@@ -20,7 +26,25 @@ public class BoosterController {
     public String getAvailableBoosterID() {
         try {
             return boosterService.getAvailableBoosterID();
-        } catch(BoosterNotAvailableException e) {
+        } catch (BoosterNotAvailableException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PostMapping()
+    public void createBooster(@RequestBody Booster booster) {
+        try {
+            boosterService.addNewBooster(booster);
+        } catch (CannotBeNullException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public Booster lookForBooster(@PathVariable String id) {
+        try {
+            return boosterService.retrieveBooster(id);
+        } catch (BoosterDoesNotExistException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
         }
     }
@@ -29,6 +53,5 @@ public class BoosterController {
     public String ping() {
         return "ok";
     }
-
 
 }
--- team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/BoosterService.java
+++ team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/BoosterService.java
@@ -6,20 +6,38 @@ import java.util.List;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.mocks.BoostersMocked;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.Booster;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.BoosterStatus;
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.BoosterDoesNotExistException;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.BoosterNotAvailableException;
-
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.CannotBeNullException;
 
 @Service
 public class BoosterService {
 
-    public void updateAllBoostersState(){
+    public void addNewBooster(Booster booster) throws CannotBeNullException {
+        if (booster == null) {
+            throw new CannotBeNullException("Booster");
+        }
+        booster.initStatus();
+        BoostersMocked.boosters.add(booster);
+    }
+
+    public Booster retrieveBooster(String boosterId) throws BoosterDoesNotExistException {
+        for (Booster b : BoostersMocked.boosters) {
+            if (b.getId().equals(boosterId)) {
+                return b;
+            }
+        }
+        throw new BoosterDoesNotExistException(boosterId);
+    }
+
+    public void updateAllBoostersState() {
         List<Booster> boosters = BoostersMocked.getAll();
-        for(Booster b : boosters){
+        for (Booster b : boosters) {
             b.updateState();
         }
     }
 
-    public String getAvailableBoosterID() throws BoosterNotAvailableException{
+    public String getAvailableBoosterID() throws BoosterNotAvailableException {
         List<Booster> boosters = BoostersMocked.getAll();
         for (Booster b : boosters) {
             if (b.getStatus() == BoosterStatus.READY) {
