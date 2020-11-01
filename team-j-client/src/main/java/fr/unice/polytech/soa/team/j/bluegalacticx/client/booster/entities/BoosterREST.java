05d9288a369ef8e07a63aef222ce843a41d37402
Betsara-MARCELLIN <43007842+Betsara-MARCELLIN@users.noreply.github.com>
2020-11-01 22:38:21 +0100

    #88 fixing tests

--- team-j-client/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/client/booster/entities/BoosterREST.java
+++ team-j-client/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/client/booster/entities/BoosterREST.java
@@ -1,12 +1,25 @@
 package fr.unice.polytech.soa.team.j.bluegalacticx.client.booster.entities;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+
+import fr.unice.polytech.soa.team.j.bluegalacticx.client.JsonUtils;
 import fr.unice.polytech.soa.team.j.bluegalacticx.client.RestAPI;
 
-public class BoosterREST extends RestAPI{
+public class BoosterREST extends RestAPI {
 
     public BoosterREST(String uri) {
         super(uri);
-        
     }
-     
+
+    public void createBooster(Booster booster) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().POST(BodyPublishers.ofString(JsonUtils.toJson(booster)))
+                .header("Content-Type", "application/json").uri(URI.create(uri)).build();
+
+        client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
 }
--- team-j-client/src/test/java/fr/unice/polytech/soa/team/j/bluegalacticx/client/manage_multiple_rocket.java
+++ team-j-client/src/test/java/fr/unice/polytech/soa/team/j/bluegalacticx/client/manage_multiple_rocket.java
@@ -1,9 +1,13 @@
 package fr.unice.polytech.soa.team.j.bluegalacticx.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import fr.unice.polytech.soa.team.j.bluegalacticx.client.booster.entities.Booster;
 import fr.unice.polytech.soa.team.j.bluegalacticx.client.booster.entities.BoosterREST;
+import fr.unice.polytech.soa.team.j.bluegalacticx.client.mission.MissionREST;
+import fr.unice.polytech.soa.team.j.bluegalacticx.client.mission.entities.Mission;
 import fr.unice.polytech.soa.team.j.bluegalacticx.client.rocket.RocketREST;
 import fr.unice.polytech.soa.team.j.bluegalacticx.client.rocket.RocketRPC;
 import fr.unice.polytech.soa.team.j.bluegalacticx.client.rocket.entities.Rocket;
@@ -19,6 +23,7 @@ public class manage_multiple_rocket implements En {
     private RocketREST rocketREST = new RocketREST("http://localhost:8080/rocket");
     private BoosterREST boosterREST = new BoosterREST("http://localhost:8080/booster");
     private RocketRPC rocketRPC = new RocketRPC("localhost", 8081);
+    private MissionREST missionREST = new MissionREST("http://localhost:8080/mission");
 
     public manage_multiple_rocket() {
         Given("Elon who added the rockets below :", (DataTable dataTable) -> {
@@ -28,25 +33,43 @@ public class manage_multiple_rocket implements En {
                 rocketREST.createRocket(new Rocket().id(columns.get(0)).objective(stringToCoordinate(columns.get(1)))
                         .boosterId(columns.get(2)));
             }
+
+            for (List<String> columns : rows) {
+                assertNotNull(rocketREST.getStatus(columns.get(0)));
+            }
         });
 
-        // TODO create boosters
-        // Given("the boosters below :", (DataTable dataTable) -> {
-        // });
+        And("the boosters below :", (DataTable dataTable) -> {
+            List<List<String>> rows = dataTable.asLists(String.class);
 
-        Given("Elon get the authorisation to launch the rocket {string}", (String string) -> {
-            assertTrue(true);
+            for (List<String> columns : rows) {
+                boosterREST.createBooster(new Booster().id(columns.get(0)).fuelLevel(Integer.parseInt(columns.get(1))));
+            }
         });
+
+        And("Richard programs the following missions", (DataTable dataTable) -> {
+            List<List<String>> rows = dataTable.asLists(String.class);
+            for (List<String> columns : rows) {
+                missionREST.createNewMission(new Mission().id(columns.get(0)).rocketId(columns.get(1)));
+            }
+        });
+
+        Given("Elon get the authorisation to launch the rocket {string} for the mission {string}",
+                (String string, String string2) -> {
+                    rocketRPC.setReadyToLaunch(string, string2);
+                });
+
         When("Elon launches the rocket {string}", (String string) -> {
-            // Write code here that turns the phrase above into concrete actions
             rocketRPC.LaunchOrderRequest(true, string);
         });
+
         When("{int} time unit passed", (Integer int1) -> {
             TimeUnit.SECONDS.sleep(10);
         });
+
         Then("the rocket {string} is {string}", (String string, String string2) -> {
             RocketStatus rs = rocketREST.getStatus(string);
-            assertEquals(rs.toString(), string2);
+            assertEquals(string2, rs.toString());
         });
     }
 
@@ -55,4 +78,5 @@ public class manage_multiple_rocket implements En {
         return new SpaceCoordinate(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),
                 Integer.parseInt(tokens[2]));
     }
+
 }
