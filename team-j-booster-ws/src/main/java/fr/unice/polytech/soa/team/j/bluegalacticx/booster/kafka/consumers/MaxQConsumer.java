b0a4fa9cb57c41b599deda705dda0304a63a64d1
JYassine <yassine.jrad@etu.unice.fr>
2020-11-07 22:47:59 +0100

    #150 delete mocks on booster

--- team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/kafka/consumers/MaxQConsumer.java
+++ team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/kafka/consumers/MaxQConsumer.java
@@ -1,24 +1,29 @@
 package fr.unice.polytech.soa.team.j.bluegalacticx.booster.kafka.consumers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.BoosterService;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.SpeedChange;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.mocks.BoostersMocked;
+import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.exceptions.BoosterDoesNotExistException;
 import fr.unice.polytech.soa.team.j.bluegalacticx.rocket.proto.MaxQRequest;
 
 @Service
 public class MaxQConsumer {
 
+    @Autowired
+    BoosterService boosterService;
+
     @KafkaListener(topics = "${kafka.topics.maxq}", groupId = "${kafka.group.default}", containerFactory = "maxQKafkaListenerContainerFactory")
-    public void maxQEvent(MaxQRequest request) {
+    public void maxQEvent(MaxQRequest request) throws BoosterDoesNotExistException {
         String id = request.getBoosterId();
         switch (request.getEventType()) {
             case ENTER_MAXQ:
-                BoostersMocked.find(id).get().updateSpeed(SpeedChange.DECREASE);
+                boosterService.retrieveBooster(id).updateSpeed(SpeedChange.DECREASE);
                 break;
             case QUIT_MAXQ:
-                BoostersMocked.find(id).get().updateSpeed(SpeedChange.INCREASE);
+                boosterService.retrieveBooster(id).updateSpeed(SpeedChange.INCREASE);
                 break;
             default:
                 break;
--- team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/scheduled/BoosterScheduler.java
+++ team-j-booster-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/booster/scheduled/BoosterScheduler.java
@@ -1,18 +1,10 @@
 package fr.unice.polytech.soa.team.j.bluegalacticx.booster.scheduled;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.BoosterApi;
 import fr.unice.polytech.soa.team.j.bluegalacticx.booster.BoosterService;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.Booster;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.BoosterStatus;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.entities.BoosterTelemetryData;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.kafka.producers.BoosterStatusProducer;
-import fr.unice.polytech.soa.team.j.bluegalacticx.booster.kafka.producers.TelemetryBoosterProducer;
 
 @Component
 public class BoosterScheduler {
@@ -20,32 +12,12 @@ public class BoosterScheduler {
     @Autowired
     private BoosterService boosterService;
 
-    @Autowired
-    private BoosterApi boosterApi;
-
-    @Autowired
-    private TelemetryBoosterProducer telemetryBoosterProd;
-
-    @Autowired
-    BoosterStatusProducer boosterStatusProducer;
-
     @Scheduled(fixedDelay = 1000)
     public void scheduleBoosterMetricsTask() {
         boosterService.updateAllBoostersState();
-        List<Booster> boosters = boosterApi.updateBoosterMetricsAndRetrieve();
-        for (Booster b : boosters) {
-            if (b.getStatus() == BoosterStatus.RUNNING || b.getStatus() == BoosterStatus.LANDING) {
-                telemetryBoosterProd.postTelemetryEvent(new BoosterTelemetryData(b.getFuelLevel(), b.getId(),
-                        b.getStatus(), b.getDistanceFromEarth(), b.getSpeed()));
-            } else if (b.getStatus() == BoosterStatus.LANDED) {
-                telemetryBoosterProd.postTelemetryEvent(new BoosterTelemetryData(b.getFuelLevel(), b.getId(),
-                        b.getStatus(), b.getDistanceFromEarth(), b.getSpeed()));
-                if (b.getStatus() != BoosterStatus.PENDING) {
-                    boosterStatusProducer.notifyBoosterLanded(b.getId());
-                }
-                b.setStatus(BoosterStatus.PENDING);
-            }
-        }
+        boosterService.updateAllBoostersTelemetry();
+        boosterService.updateTelemetryToSend();
+
     }
 
 }
