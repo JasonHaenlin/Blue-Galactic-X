785c9c051ecf6f0389471d3f4345a24b8408d4da
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-11-09 21:33:08 +0100

    [#160] rework payload and remove telemetry

--- team-j-missionlogwriter-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/missionlogwriter/MissionLogService.java
+++ team-j-missionlogwriter-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/missionlogwriter/MissionLogService.java
@@ -3,18 +3,19 @@ package fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter.proto.PayloadStatusRequest;
 import fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter.db.MissionLogRepository;
 import fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter.entities.MissionLog;
 import fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter.exceptions.MissionNotFoundException;
+import fr.unice.polytech.soa.team.j.bluegalacticx.missionlogwriter.proto.PayloadStatusRequest;
 
 @Service
 public class MissionLogService {
 
     @Autowired
     private MissionLogRepository missionLogRepository;
-    
-    public void createMissionLogData(PayloadStatusRequest payloadStatusData, String log) throws MissionNotFoundException {
+
+    public void createMissionLogData(PayloadStatusRequest payloadStatusData, String log)
+            throws MissionNotFoundException {
         String missionId = payloadStatusData.getMissionId();
         if (missionId == null) {
             throw new MissionNotFoundException();
---
+++ team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/kafka/producers/PayloadCreateProducer.java
@@ -0,0 +1,30 @@
+package fr.unice.polytech.soa.team.j.bluegalacticx.payload.kafka.producers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.entities.Payload;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.proto.PayloadRequest;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.proto.PayloadRequest.SpaceCoordinate;
+
+@Service
+public class PayloadCreateProducer {
+
+    @Value(value = "${kafka.topics.newpayload}")
+    private String statusTopic0;
+
+    @Autowired
+    private KafkaTemplate<String, PayloadRequest> kafkaTemplate;
+
+    public void notifyNewPayload(Payload p) {
+        PayloadRequest req = PayloadRequest.newBuilder().setPayloadId(p.getId()).setMissionId(p.getMissionId())
+                .setPayloadStatus(p.getStatus()).setPosition(SpaceCoordinate.newBuilder().setX(p.getPosition().getX())
+                        .setY(p.getPosition().getY()).setZ(p.getPosition().getZ()).build())
+                .build();
+
+        kafkaTemplate.send(statusTopic0, req);
+    }
+
+}
