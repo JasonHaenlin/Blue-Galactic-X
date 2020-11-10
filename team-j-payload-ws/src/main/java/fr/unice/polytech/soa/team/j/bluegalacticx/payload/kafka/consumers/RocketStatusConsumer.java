e5ef20cea709d458309d830fde9de9c8cb2d7b20
JYassine <yassine.jrad@etu.unice.fr>
2020-11-10 21:30:08 +0100

    #175 Send ON_MISSION status

--- team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/kafka/consumers/RocketStatusConsumer.java
+++ team-j-payload-ws/src/main/java/fr/unice/polytech/soa/team/j/bluegalacticx/payload/kafka/consumers/RocketStatusConsumer.java
@@ -5,7 +5,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import fr.unice.polytech.soa.team.j.bluegalacticx.payload.PayloadService;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.entities.Payload;
 import fr.unice.polytech.soa.team.j.bluegalacticx.payload.exceptions.PayloadNotFoundException;
+import fr.unice.polytech.soa.team.j.bluegalacticx.payload.kafka.producers.PayloadStatusProducer;
 import fr.unice.polytech.soa.team.j.bluegalacticx.payload.proto.PayloadStatus;
 import fr.unice.polytech.soa.team.j.bluegalacticx.rocket.proto.RocketStatusRequest;
 
@@ -15,6 +17,9 @@ public class RocketStatusConsumer {
     @Autowired
     private PayloadService payloadService;
 
+    @Autowired
+    private PayloadStatusProducer payloadStatusProducer;
+
     @KafkaListener(topics = "${kafka.topics.rocketstatus}", groupId = "${kafka.group.default}", containerFactory = "rocketStatusKafkaListenerContainerFactory")
     public void rocketStatusEvent(RocketStatusRequest request) {
         String id = request.getRocketId();
@@ -22,6 +27,10 @@ public class RocketStatusConsumer {
             switch (request.getEventType()) {
                 case IN_SERVICE:
                     payloadService.updatePayloadFromRocketState(PayloadStatus.ON_MISSION, id);
+                    Payload p = payloadService.retrievePayloadByRocketId(id);
+                    p.setStatus(PayloadStatus.ON_MISSION);
+                    payloadStatusProducer.notifyOnMissionPayloadEvent(p.getId(), p.getMissionId());
+
                     break;
                 case DESTROYED:
                     payloadService.updatePayloadFromRocketState(PayloadStatus.DESTROYED, id);
